f71a35cd4161a3f122d6409bbed79a02bb7eddd9
SOIX <sasa@infinity-x.tech>
2023-05-15 22:24:08 +0100

    New properties client

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      Search
+    </div>
+  </header>
+
+  <main>
+    <h1>New App</h1>
+  </main>
+</template>
